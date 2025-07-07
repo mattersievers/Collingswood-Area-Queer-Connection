178c29f77a36a3b62813d8deb36b331cf06ba67d
Matthew Sievers <mattersievers@gmail.com>
2025-07-07 12:55:49 -0400

    edited react homepage and added comp and asset folders

--- collingswood-area-queer-connection/src/App.js
+++ collingswood-area-queer-connection/src/App.js
@@ -5,18 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Collingswood Area Queer Connection</h1>
+       <div>
+        <p>Here is our brief story.</p>
+       </div>
       </header>
     </div>
   );
