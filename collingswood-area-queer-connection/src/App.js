62e3a1271d5e84389a67394a055b106e313ff748
Matthew Sievers <mattersievers@gmail.com>
2025-07-07 13:08:01 -0400

    fixed folder location for assets/components

--- collingswood-area-queer-connection/src/App.js
+++ collingswood-area-queer-connection/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
        <h1>Collingswood Area Queer Connection</h1>
        <div>
---
+++ collingswood-area-queer-connection/src/components/Navbar.js
@@ -0,0 +1,7 @@
+const AppNavBar = () =>{
+    return(
+        <div>Here is the Nav</div>
+    )
+}
+
+export default AppNavBar;
