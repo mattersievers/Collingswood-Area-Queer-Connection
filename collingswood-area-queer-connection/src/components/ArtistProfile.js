4ae751ccf6093127ad7fdd620138129faf5ea044
Matthew Sievers <mattersievers@gmail.com>
2025-07-11 15:48:39 -0400

    generalized file name for photo and added photo map to profile

--- collingswood-area-queer-connection/src/components/ArtistProfile.js
+++ collingswood-area-queer-connection/src/components/ArtistProfile.js
@@ -1,14 +1,16 @@
 //I need to count the number of photos in the local repo and print that many img in a loop
 
 const ArtistProfile = (props) =>{
-    
     return(
         <>
             <h1>{props.currentArtist.name}</h1>
-            <img className="profilePhoto" src={require(`../assets/artists/${props.currentArtist.name}/photo_1.jpg`)} alt={props.currentArtist.name} />
+            <img className="profilePhoto" src={require(`../assets/artists/${props.currentArtist.name}/${props.currentArtist.profilePhoto}`)} alt={props.currentArtist.name} />
             <h2>{props.currentArtist.specialty}</h2>
             <div>Maybe we'll put some work in here</div>
             <div>And A brief synopsis about what they do</div>
+            {props.currentArtist.artPhotoList.map((artPhotoFile,i) => {
+                return(<img className="profilePhoto" src={require(`../assets/artists/${props.currentArtist.name}/${artPhotoFile}`)} alt={artPhotoFile} />
+            )})}
             
         </>
     )
--- collingswood-area-queer-connection/src/utils/artistListDetails.js
+++ collingswood-area-queer-connection/src/utils/artistListDetails.js
@@ -3,31 +3,41 @@ const artists = [
         name: "Alfred O'Neil Hitchcock III",
         pronouns: "they/them",
         specialty:"painting",
-        url:"www.amorphousblob.com"
+        url:"www.amorphousblob.com",
+        profilePhoto: "photo_1.jpg",
+        artPhotoList: ["photo_2.jpg","photo_3.jpg","photo_4.jpg"]
     },
      {
         name: "Blernie Standers",
         pronouns: "he/him",
         specialty:"skateboard paintings",
-        url:"www.boardgramps.com"
+        url:"www.boardgramps.com",
+        profilePhoto: "photo_1.jpg",
+        artPhotoList: ["photo_2.jpg","photo_3.jpg"]
     },
      {
         name: "James Beakman Nargly",
         pronouns: "ze/zim",
         specialty:"makeup/SFX",
-        url:"www.newfaceeveryday.com"
+        url:"www.newfaceeveryday.com",
+        profilePhoto: "photo_1.jpg",
+        artPhotoList: ["photo_2.jpg","photo_3.jpg","photo_4.jpg"]
     },
     {
         name: "Reginald Woolsworth",
         pronouns: "he/him",
         specialty:"welding/metal work",
-        url:"www.welderartistrule.com"
+        url:"www.welderartistrule.com",
+        profilePhoto: "photo_1.jpg",
+        artPhotoList: ["photo_2.jpg","photo_3.jpg"]
     },
      {
         name: "Tryn",
         pronouns: "she/her",
         specialty:"photography/digital editing",
-        url:"www.whataweirdworld.com"
+        url:"www.whataweirdworld.com",
+        profilePhoto: "photo_1.jpg",
+        artPhotoList: ["photo_2.jpg","photo_3.jpg","photo_4.jpg"]
     }
 ]
 
